a4f34bfe12ee024429d6aaee662f793ef9f9c51e
Sacha Nacar <sacha.slushy@gmail.com>
2015-09-03 15:37:04 +0200

    Update token.php

--- token.php
+++ token.php
@@ -20,8 +20,4 @@ $key = cleanHmacDigest(base64_encode(hash_hmac('sha1', $text, $secret, true)));
 // Structure output array
 $output = array('key'=>$key,'expires'=>$expires,'username'=>$username);
 
-// Output as JSON
-header('Content-Type: application/json');
-echo json_encode($output);
-
 ?>
